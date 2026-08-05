2dbfc49e3cb6f4e7a2bde46c7077a32c04787765
Gaurav kumar <gauravchauhan639710@gmail.com>
2026-08-05 23:03:12 +0530

    Time: 4 ms (99.95%) | Memory: 48.6 MB (50.75%) - LeetSync

--- 575-distribute-candies/distribute-candies.java
+++ 575-distribute-candies/distribute-candies.java
@@ -1,17 +1,17 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
-         // 1. Create a HashSet to store unique candy types
-        HashSet<Integer> uniqueCandies = new HashSet<>();
-        
-        // 2. Add each candy type to the set (duplicates are ignored automatically)
-        for (int candy : candyType) {
-            uniqueCandies.add(candy);
+          boolean [] visit = new boolean[200001];
+        int advised = candyType.length/2;
+       int count = 0;
+        for( int num : candyType){
+            int index = num + 100000;
+            if(!visit[index]){
+                visit[index] = true;
+                if(++count> advised){
+                    return advised;
+                }
+            }
         }
-        
-        // 3. Get the maximum candies Alice is allowed to eat
-        int maxAllowed = candyType.length / 2;
-        
-        // 4. Return the smaller value between unique types available and her eating limit
-        return Math.min(uniqueCandies.size(), maxAllowed);
+        return count;
     }
 }
